25c8bf07010d772e606e3d5d410c73d4d86093cd
Thomas Van Roost <thomasvanroost@gmail.com>
2014-11-18 13:54:08 +0100

    Homeview in seperate file

--- js/main.js
+++ js/main.js
@@ -7,26 +7,15 @@ var app = {
             alert(title ? (title + ": " + message) : message);
         }
     },
-
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
+    
     initialize: function() {
         var self = this;
+        //this.homeTpl = Handlebars.compile($("#home-tpl").html());
+		//this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        
         this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
-        });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+			$('body').html(new HomeView(self.store).render().el);
+		});        
     }
 
 };
